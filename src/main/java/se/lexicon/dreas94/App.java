80295271e1ad5a0563387c09d5f0b6a7b1d2c7dc
Andreas Eriksson <dreas94@gmail.com>
2022-03-30 10:51:11 +0200

    Added Exercises Class and finished Exercise 1

--- src/main/java/se/lexicon/dreas94/App.java
+++ src/main/java/se/lexicon/dreas94/App.java
@@ -1,13 +1,10 @@
 package se.lexicon.dreas94;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Exercises ex = new Exercises();
     }
 }
+
---
+++ src/main/java/se/lexicon/dreas94/Exercises.java
@@ -0,0 +1,11 @@
+package se.lexicon.dreas94;
+
+public class Exercises
+{
+    public void exercise1()
+    {
+        String str = "Java";
+        System.out.println("1. What is the length of the String: 'Java'?");
+        System.out.println("Answer: " + str.length());
+    }
+}
