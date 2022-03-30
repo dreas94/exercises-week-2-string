2c0744eb38d0364cc6ec603cdf0ddbb5c6dc5645
Andreas Eriksson <dreas94@gmail.com>
2022-03-30 10:54:21 +0200

    Finished Exercise 2

--- src/main/java/se/lexicon/dreas94/App.java
+++ src/main/java/se/lexicon/dreas94/App.java
@@ -5,6 +5,9 @@ public class App
     public static void main( String[] args )
     {
         Exercises ex = new Exercises();
+
+        ex.exercise1();
+        ex.exercise2();
     }
 }
 
--- src/main/java/se/lexicon/dreas94/Exercises.java
+++ src/main/java/se/lexicon/dreas94/Exercises.java
@@ -5,7 +5,14 @@ public class Exercises
     public void exercise1()
     {
         String str = "Java";
-        System.out.println("1. What is the length of the String: 'Java'?");
+        System.out.println("1. What is the length of the String: '" + str + "' ?");
         System.out.println("Answer: " + str.length());
+    }
+
+    public void exercise2()
+    {
+        String str = "Long example sentence";
+        System.out.println("1. What char is at index position 6 in the following String: '" + str + "' ?");
+        System.out.println("Answer: " + str.charAt(6));
     }
 }
