8c5a383f965ee6a1c22975bbe2793066055ed28d
Andreas Eriksson <dreas94@gmail.com>
2022-03-30 11:00:36 +0200

    Updated Exercise 1 & 2

--- src/main/java/se/lexicon/dreas94/Exercises.java
+++ src/main/java/se/lexicon/dreas94/Exercises.java
@@ -6,16 +6,16 @@ public class Exercises
 
     public void exercise1()
     {
-        String str = "Java";
+        str = "Java";
         System.out.println("1. What is the length of the String: '" + str + "' ?");
-        System.out.println("Answer: " + str.length());
+        System.out.println("Answer: " + str.length() + "\n");
     }
 
     public void exercise2()
     {
-        String str = "Long example sentence";
-        System.out.println("1. What char is at index position 6 in the following String: '" + str + "' ?");
-        System.out.println("Answer: " + str.charAt(6));
+        str = "Long example sentence";
+        System.out.println("2. What char is at index position 6 in the following String: '" + str + "' ?");
+        System.out.println("Answer: " + str.charAt(6) + "\n");
     }
 
     public void exercise3()
